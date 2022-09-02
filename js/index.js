765f8d1d290de6a95286dec79ad522dee48862e2
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-02 23:55:58 +0600

    first menu added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log('hello js');
+const loadAllMenu = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  const response = await fetch(url);
+  const data = await response.json();
+  displayAllMenu(data.data.news_category);
+};
+
+const displayAllMenu = (menu) => {
+  console.log(menu);
+    menu.forEach(element => {
+        console.log(element.category_name);
+    });
+
+};
+
+loadAllMenu();
